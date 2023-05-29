1f3c3253507d7e230fa0a9d0f38920382f1029ec
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-05-29 18:55:53 +0530

    format: Formatted code of ProgressDialogCompat

--- lib/src/main/java/ninja/corex/viewbind/progressdialogcompat/ProgressDialogCompat.java
+++ lib/src/main/java/ninja/corex/viewbind/progressdialogcompat/ProgressDialogCompat.java
@@ -9,41 +9,43 @@ import com.google.android.material.shape.ShapeAppearanceModel;
 import ninja.corex.privateutil.PrivateColorUtil;
 
 public class ProgressDialogCompat extends ProgressDialog {
-	protected MaterialShapeDrawable shape;
-	
-	public ProgressDialogCompat(Context context) {
-		super(context);
+    protected MaterialShapeDrawable shape;
 
-	}
-	public ProgressDialogCompat(Context context,int def) {
-		super(context,def);
+    public ProgressDialogCompat(Context context) {
+        super(context);
+    }
 
-	}
+    public ProgressDialogCompat(Context context, int def) {
+        super(context, def);
+    }
 
-	public static int StyleMobileDrak() {
-		return ProgressDialogCompat.THEME_DEVICE_DEFAULT_DARK;
+    public static int StyleMobileDrak() {
+        return ProgressDialogCompat.THEME_DEVICE_DEFAULT_DARK;
+    }
 
-	}
+    public int StyleMoblieLight() {
+        return ProgressDialogCompat.THEME_DEVICE_DEFAULT_LIGHT;
+    }
 
-	public int StyleMoblieLight() {
-		return ProgressDialogCompat.THEME_DEVICE_DEFAULT_LIGHT;
-	}
+    public void setShapeCut(int color, int stokerColor, float sizeShape) {
+        shape =
+                new MaterialShapeDrawable(
+                        ShapeAppearanceModel.builder()
+                                .setAllCorners(PrivateColorUtil.CornerCut, sizeShape)
+                                .build());
+        shape.setFillColor(ColorStateList.valueOf(color));
+        shape.setStroke(1f, stokerColor);
+        getWindow().getDecorView().setBackground(shape);
+    }
 
-	public void setShapeCut(int color, int stokerColor, float sizeShape) {
-		 shape = new MaterialShapeDrawable(
-				ShapeAppearanceModel.builder().setAllCorners(PrivateColorUtil.CornerCut, sizeShape).build());
-		shape.setFillColor(ColorStateList.valueOf(color));
-		shape.setStroke(1f, stokerColor);
-		getWindow().getDecorView().setBackground(shape);
-	}
-
-	public void setShapeRounde(int color, int stoker, float sizeShape) {
-		 shape = new MaterialShapeDrawable(
-				ShapeAppearanceModel.builder().setAllCorners(PrivateColorUtil.CornerNormal, sizeShape).build());
-		shape.setFillColor(ColorStateList.valueOf(color));
-		shape.setStroke(1f, stoker);
-		getWindow().getDecorView().setBackground(shape);
-	}
-	
-	
+    public void setShapeRounde(int color, int stoker, float sizeShape) {
+        shape =
+                new MaterialShapeDrawable(
+                        ShapeAppearanceModel.builder()
+                                .setAllCorners(PrivateColorUtil.CornerNormal, sizeShape)
+                                .build());
+        shape.setFillColor(ColorStateList.valueOf(color));
+        shape.setStroke(1f, stoker);
+        getWindow().getDecorView().setBackground(shape);
+    }
 }
