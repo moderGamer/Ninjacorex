bc61f053d7c9256d6ed7a1a1dee2467616a4057e
* <modergamer99@gmail.com>
2023-06-01 09:57:45 +0330

    Add Animation from xml

--- lib/src/main/java/ninja/corex/animation/AnimationUtils.java
+++ lib/src/main/java/ninja/corex/animation/AnimationUtils.java
@@ -3,53 +3,79 @@ package ninja.corex.animation;
 import android.os.Handler;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.ScaleAnimation;
+import ninja.corex.R;
 
 public class AnimationUtils {
-	private static AlphaAnimation alphaAnimation;
-	private static Handler handler;
-	private static ScaleAnimation scaleAnimation;
-	
+  private static AlphaAnimation alphaAnimation;
+  private static Handler handler;
+  private static ScaleAnimation scaleAnimation;
 
-	public static void FadeIn(View view) {
-		alphaAnimation = new AlphaAnimation(1, 0);
-		alphaAnimation.setDuration(1000);
-		alphaAnimation.setFillAfter(true);
-		alphaAnimation.setInterpolator(new BounceInterpolator());
-		handler = new Handler();
-		handler.postDelayed(() -> {
-			view.startAnimation(alphaAnimation);
-		}, 100);
-	}
+  public static void FadeIn(View view) {
+    alphaAnimation = new AlphaAnimation(1, 0);
+    alphaAnimation.setDuration(1000);
+    alphaAnimation.setFillAfter(true);
+    alphaAnimation.setInterpolator(new BounceInterpolator());
+    handler = new Handler();
+    handler.postDelayed(
+        () -> {
+          view.startAnimation(alphaAnimation);
+        },
+        100);
+  }
 
-	public static void FadeCustom(View view, float initem, float outitem, int Timer) {
-		alphaAnimation = new AlphaAnimation(initem, outitem);
-		alphaAnimation.setDuration(Timer);
-		alphaAnimation.setFillAfter(true);
-		handler = new Handler();
-		handler.postDelayed(() -> {
-			view.startAnimation(alphaAnimation);
-		}, 100);
-	}
+  public static void FadeCustom(View view, float initem, float outitem, int Timer) {
+    alphaAnimation = new AlphaAnimation(initem, outitem);
+    alphaAnimation.setDuration(Timer);
+    alphaAnimation.setFillAfter(true);
+    handler = new Handler();
+    handler.postDelayed(
+        () -> {
+          view.startAnimation(alphaAnimation);
+        },
+        100);
+  }
 
-	public static void FadeOut(View view) {
-		alphaAnimation = new AlphaAnimation(0, 1);
-		alphaAnimation.setDuration(1000);
-		alphaAnimation.setFillAfter(true);
-		handler = new Handler();
-		handler.postDelayed(() -> {
-			view.startAnimation(alphaAnimation);
-		}, 100);
-	}
+  public static void FadeOut(View view) {
+    alphaAnimation = new AlphaAnimation(0, 1);
+    alphaAnimation.setDuration(1000);
+    alphaAnimation.setFillAfter(true);
+    handler = new Handler();
+    handler.postDelayed(
+        () -> {
+          view.startAnimation(alphaAnimation);
+        },
+        100);
+  }
 
-	public static void Sacla(View view, float fromDeltax, float fromDeltay, float novPartX, float notPartY, long Time) {
-		scaleAnimation = new ScaleAnimation(fromDeltax, fromDeltay, notPartY, novPartX);
-		scaleAnimation.setFillAfter(true);
-		scaleAnimation.setDuration(Time);
-		handler.postDelayed(() -> {
-			view.startAnimation(scaleAnimation);
-		}, 100);
-	}
+  public static void Sacla(
+      View view, float fromDeltax, float fromDeltay, float novPartX, float notPartY, long Time) {
+    scaleAnimation = new ScaleAnimation(fromDeltax, fromDeltay, notPartY, novPartX);
+    scaleAnimation.setFillAfter(true);
+    scaleAnimation.setDuration(Time);
+    handler.postDelayed(
+        () -> {
+          view.startAnimation(scaleAnimation);
+        },
+        100);
+  }
 
+  public static void FadeinFromXml(View view) {
+    Animation anim = AnimationUtils.loadAnimation(view.getContext(), R.anim.fadeinCompat);
+    if (anim != null) {
+      view.setAnimation(anim);
+      anim.start();
+    }
+  }
+
+  public static void FadeOutFromXml(View view) {
+    Animation anim = AnimationUtils.loadAnimation(view.getContext(), R.anim.fadeoutCompat);
+    if (anim != null) {
+      view.setAnimation(anim);
+      anim.start();
+    }
+  }
 }
---
+++ lib/src/main/java/ninja/corex/fileselector/ad_file.java
@@ -0,0 +1,4 @@
+package ninja.corex.fileselector;
+
+public class ad_file extends Object {
+}
